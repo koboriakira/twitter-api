0e105e2cd3588cdc24886f49f5ae691f41a10767
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-03-26 01:05:32 +0900

    CDKのスタック名を変更しました***

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkStack } from '../lib/twitter-api';
+import { TwitterApi } from '../lib/twitter-api';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new TwitterApi(app, 'TwitterApi', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
