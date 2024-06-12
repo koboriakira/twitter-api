39fee93499b0a6fbf32fffe5eea102471c4394b3
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-06-13 00:30:26 +0900

    Refactor tweet module imports and organize code structure

--- src/interface/fastapi_router/tweet/tweet.py
+++ src/interface/fastapi_router/tweet/tweet.py
@@ -1,12 +1,12 @@
-from fastapi import APIRouter, Header
-from interface.fastapi_router.base_response import BaseResponse
-from use_case.find_tweet_use_case import FindTweetUseCase
+from fastapi import APIRouter
+
 from common.infrastructure.twikit_client import Twikit
 from common.value.tweet_id import TweetId
 from interface.fastapi_router.tweet.tweet_response import (
     TweetModelTranslator,
     TweetResponse,
 )
+from use_case.find_tweet_use_case import FindTweetUseCase
 
 router = APIRouter()
 
--- src/use_case/find_tweet_use_case.py
+++ src/use_case/find_tweet_use_case.py
@@ -1,10 +1,9 @@
-from common.infrastructure.twikit_client import Twikit
 from common.domain.tweet import Tweet
+from common.infrastructure.twikit_client import Twikit
 from common.value.tweet_id import TweetId
 
 
 class FindTweetUseCase:
-
     def __init__(self, twitter_client: Twikit):
         self._twitter_client = twitter_client
 
