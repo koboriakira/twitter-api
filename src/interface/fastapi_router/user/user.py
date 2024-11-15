48367b38131d5aa722212d1e2062b82294355cb4
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-15 13:50:40 +0900

    ツイート取得メソッドに開始日時と終了日時のオプション引数を追加し、指定した期間のツイートを取得できるように更新

--- src/interface/fastapi_router/user/user.py
+++ src/interface/fastapi_router/user/user.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from fastapi import APIRouter
 
@@ -14,15 +14,19 @@ router = APIRouter()
 
 
 @router.get("/{user_screen_name}/tweets/")
-async def get_tweet(user_screen_name: str) -> TweetsResponse:
+async def get_tweet(
+    user_screen_name: str,
+    start: str|None = None,
+    end: str|None = None,
+    ) -> TweetsResponse:
     """指定したユーザーの当日のツイートを取得します"""
     try:
         twitter_client = await Twikit.generate_instance()
         use_case = ListTweetsUseCase(twitter_client=twitter_client)
         tweets = await use_case.execute(
             user_screen_name=user_screen_name,
-            start_datetime=jst_today_datetime() - timedelta(days=1),
-            end_datetime=jst_today_datetime(),
+            start_datetime=datetime.fromisoformat(start) if start is not None else jst_today_datetime() - timedelta(days=1),
+            end_datetime=datetime.fromisoformat(end) if end is not None else jst_today_datetime(),
         )
         tweet_models = [TweetModelTranslator.translate(tweet) for tweet in tweets]
         return TweetsResponse(data=tweet_models)
