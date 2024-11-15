8d73bf86c191d91ca2a1449a8e24fcfc563203da
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-15 12:18:55 +0900

    非同期処理を追加し、ツイート取得メソッドを更新

--- src/use_case/find_tweet_use_case.py
+++ src/use_case/find_tweet_use_case.py
@@ -7,5 +7,5 @@ class FindTweetUseCase:
     def __init__(self, twitter_client: Twikit):
         self._twitter_client = twitter_client
 
-    def execute(self, tweet_id: TweetId) -> Tweet:
-        return self._twitter_client.find_tweet_by_id(tweet_id)
+    async def execute(self, tweet_id: TweetId) -> Tweet:
+        return await self._twitter_client.find_tweet_by_id(tweet_id)
--- src/use_case/list_tweets_use_case.py
+++ src/use_case/list_tweets_use_case.py
@@ -1,3 +1,4 @@
+import asyncio
 from datetime import datetime
 
 from common.domain.tweets import Tweets
@@ -9,22 +10,26 @@ class ListTweetsUseCase:
     def __init__(self, twitter_client: Twikit) -> None:
         self._twitter_client = twitter_client
 
-    def execute(
+    async def execute(
         self,
         user_screen_name: str,
         start_datetime: datetime | None = None,
         end_datetime: datetime | None = None,
     ) -> Tweets:
-        tweets = self._twitter_client.get_tweets_by_screen_name(user_screen_name)
+        tweets = await self._twitter_client.get_tweets_by_screen_name(user_screen_name)
         return tweets.filter_range(start_datetime, end_datetime)
 
-
-if __name__ == "__main__":
-    # python -m src.use_case.list_tweets_use_case
-    usecase = ListTweetsUseCase(twitter_client=Twikit.generate_instance())
-    tweets = usecase.execute(
+async def main():
+    twitter_client = await Twikit.generate_instance()
+    usecase = ListTweetsUseCase(twitter_client=twitter_client)
+    tweets = await usecase.execute(
         user_screen_name="kobori_akira_pw",
         start_datetime=dateutil.jst_datetime(year=2024, month=6, day=9),
         end_datetime=dateutil.jst_datetime(year=2024, month=6, day=10),
     )
     print([tweet.embed_tweet_url for tweet in tweets])
+
+
+if __name__ == "__main__":
+    # python -m src.use_case.list_tweets_use_case
+    asyncio.run(main())
