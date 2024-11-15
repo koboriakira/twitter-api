55766e83cfb2ee3f3ee65fde9fe3bacc5fa7de24
a-kobori <89172056+a-kobori@users.noreply.github.com>
2024-11-15 16:48:05 +0900

    Refactor tweet retrieval method to improve date handling with optional start and end parameters

--- src/interface/fastapi_router/user/user.py
+++ src/interface/fastapi_router/user/user.py
@@ -16,17 +16,25 @@ router = APIRouter()
 @router.get("/{user_screen_name}/tweets/")
 async def get_tweet(
     user_screen_name: str,
-    start: str|None = None,
-    end: str|None = None,
-    ) -> TweetsResponse:
+    start: str | None = None,
+    end: str | None = None,
+) -> TweetsResponse:
     """指定したユーザーの当日のツイートを取得します"""
     try:
+        start_datetime = (
+            datetime.fromisoformat(start)
+            if start is not None
+            else jst_today_datetime() - timedelta(days=1)
+        )
+        end_datetime = (
+            datetime.fromisoformat(end) if end is not None else jst_today_datetime()
+        )
         twitter_client = await Twikit.generate_instance()
         use_case = ListTweetsUseCase(twitter_client=twitter_client)
         tweets = await use_case.execute(
             user_screen_name=user_screen_name,
-            start_datetime=datetime.fromisoformat(start) if start is not None else jst_today_datetime() - timedelta(days=1),
-            end_datetime=datetime.fromisoformat(end) if end is not None else jst_today_datetime(),
+            start_datetime=start_datetime,
+            end_datetime=end_datetime,
         )
         tweet_models = [TweetModelTranslator.translate(tweet) for tweet in tweets]
         return TweetsResponse(data=tweet_models)
